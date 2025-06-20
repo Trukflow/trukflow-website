450988fd9a39f5d52d8a0a2642daed6a94114b86
Montanah <mititi.isaac@gmail.com>
2025-06-20 14:40:31 +0300

    About page edit

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -120,7 +120,7 @@ const About = () => {
                 <img
                   src={member.image}
                   alt={member.name}
-                  className="w-full h-40 object-cover"
+                  className="w-full h-80 object-cover"
                 />
                 <div className="p-6">
                   <h3 className="text-xl font-bold text-gray-900 mb-2">
